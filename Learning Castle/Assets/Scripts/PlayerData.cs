cfc07641b201bd6cfa5de3ce096db1bacf624496
BlueSun <whitepineapple69@gmail.com>
2018-03-03 19:37:24 +0300

    object containing info about player

---
+++ Learning Castle/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerData : MonoBehaviour {
+
+	public Gender gender;
+	public bool[] isCompleted;
+
+	public enum Gender {Woman, Man};
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(gameObject);
+	}
+
+	void login() {
+		SceneManager.LoadScene ("Map");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
