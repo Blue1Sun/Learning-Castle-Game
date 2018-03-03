2d627c7c8d3a914b37885a05abb130cf5df27b8b
BlueSun <whitepineapple69@gmail.com>
2018-03-03 19:54:08 +0300

    preparitions for test development

--- Learning Castle/Assets/Scripts/Test.cs
+++ Learning Castle/Assets/Scripts/Test.cs
@@ -1,11 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Test : MonoBehaviour {
 
-	public QType qtype;
+	public class Question
+	{
+		public int type; //1 - oneA, 2 - manyA, 3 - textA
+		public string question;
+		public string[] answers;
+
+		public Question(int type, string question, string[] answers)
+		{
+			this.type = type;
+			this.question = question;
+			this.answers = answers;
+		}
+	};
+
+	public QType qtype; //сделать так чтобы брался тип след/пред вопроса
+	public int numOfQuestions = 10;
 	public enum QType {
 		oneA,
 		manyA,
@@ -14,9 +31,13 @@ public class Test : MonoBehaviour {
 
 	private GameObject toggles;
 	private GameObject longAnsw;
+	private int curQuestion;
+	private string[] playerAnswers;
 
 	// Use this for initialization
 	void Start () {
+		Question[] questions; //массив классов - одна тема
+		curQuestion = 0;
 		toggles = GameObject.Find ("Toggles");
 		longAnsw = GameObject.Find ("LongAnswer");
 
@@ -28,19 +49,36 @@ public class Test : MonoBehaviour {
 		
 	}
 
-	public void NextQuestion() {
+	public void PrevQuestion(){
+		curQuestion--;
+		changeLayout (qtype);
+	}
 
+	public void NextQuestion() {
+		curQuestion++;
+		if (curQuestion > numOfQuestions)
+			SceneManager.LoadScene ("Menu");
+		else
+			changeLayout (qtype);
+	}
+
+	void changeLayout(QType qtype)
+	{
 		Toggle answer1 = null;
 		Toggle answer2 = null;
 		Toggle answer3 = null;
 		Toggle answer4 = null;
 
-		if (qtype == QType.manyA) {
-			toggles.SetActive(true);
+		if (qtype == QType.manyA || qtype == QType.oneA) {
+			toggles.SetActive (true);
 			longAnsw.SetActive (false);
-
 			findToggles (ref answer1, ref answer2, ref answer3, ref answer4);
+		} else {
+			toggles.SetActive(false);
+			longAnsw.SetActive (true);
+		}
 
+		if (qtype == QType.manyA) {
 			answer1.isOn = answer2.isOn = answer3.isOn = answer4.isOn = false;
 
 			answer1.group = null;
@@ -49,11 +87,7 @@ public class Test : MonoBehaviour {
 			answer4.group = null;
 		} 
 		else if (qtype == QType.oneA) {
-			toggles.SetActive(true);
-			longAnsw.SetActive (false);
 			ToggleGroup tg = GameObject.Find ("Toggles").GetComponent<ToggleGroup> ();
-
-			findToggles (ref answer1, ref answer2, ref answer3, ref answer4);
 
 			answer1.isOn = true;
 			answer2.isOn = answer3.isOn = answer4.isOn = false;
@@ -63,9 +97,22 @@ public class Test : MonoBehaviour {
 			answer3.group = tg;
 			answer4.group = tg;
 		} 
-		else {
-			toggles.SetActive(false);
-			longAnsw.SetActive (true);
+
+		GameObject bBack = GameObject.Find ("Back");
+		GameObject bNext = GameObject.Find ("Next");
+
+		if (curQuestion == 1) {			
+			bBack.GetComponent<Button> ().interactable = false;
+			bBack.GetComponentInChildren<Text> ().color = new Color (0.2f, 0.2f, 0.2f, 0.5f);
+		} else {
+			bBack.GetComponent<Button> ().interactable = true;
+			bBack.GetComponentInChildren<Text> ().color = new Color (0.2f, 0.2f, 0.2f, 1f);
+		}
+
+		if (curQuestion == numOfQuestions) {
+			bNext.GetComponentInChildren<Text> ().text = "Завершить";
+		} else {
+			bNext.GetComponentInChildren<Text> ().text = "Далее";
 		}
 	}
 
