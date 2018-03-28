44bb98ff6d0c46a5dfb4f49ff702d3ef3d695f39
BlueSun <whitepineapple69@gmail.com>
2018-03-28 17:10:36 +0300

    New english minigame preparation

---
+++ Learning Castle/Assets/Scripts/EngGuitarGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngGuitarGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		GameObject.Find ("LeftArrSpr").GetComponent<Animator> ().Play ("Flying");
+		GameObject.Find ("RightArrSpr").GetComponent<Animator> ().Play ("Flying");
+		GameObject.Find ("DownArrSpr").GetComponent<Animator> ().Play ("Flying");
+		GameObject.Find ("UpArrSpr").GetComponent<Animator> ().Play ("Flying");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
