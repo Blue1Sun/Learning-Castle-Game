85f550825d2bd01fcf6629102565fc1e222a8975
BlueSun <whitepineapple69@gmail.com>
2018-03-28 17:09:01 +0300

    Function toString were added

--- Learning Castle/Assets/Scripts/PlayerData.cs
+++ Learning Castle/Assets/Scripts/PlayerData.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerData : MonoBehaviour {
-
-	public Gender gender;
+	
+	public int id;
+	public int status;
+	public string gender;
 	public bool[] isCompleted;
 	public bool[] completedTests;
 	public int[] minigameRecord;
-
-	public enum Gender {Woman, Man};
 
 	void Start () {
 		DontDestroyOnLoad(gameObject);
@@ -18,5 +18,25 @@ public class PlayerData : MonoBehaviour {
 
 	void login() {
 		SceneManager.LoadScene ("Map");
+	}
+
+	public string toString(){
+		string userInformation = "";
+
+		userInformation += this.id+ " " + this.status+ " " + this.gender;
+
+		foreach (bool unit in isCompleted)
+			userInformation += unit + " ";
+		//userInformation += "\r\n";
+
+		foreach (bool unit in completedTests)
+			userInformation += unit + " ";
+		//userInformation += "\r\n";
+
+		foreach (int unit in minigameRecord)
+			userInformation += unit + " ";
+		//userInformation += "\r\n";
+
+		return userInformation;
 	}
 }
