a3f006530c46e6eb3b0036af006d2d66180105e0
BlueSun <whitepineapple69@gmail.com>
2018-03-28 16:12:03 +0300

    Little connection with server were added

--- Learning Castle/Assets/Scripts/Test.cs
+++ Learning Castle/Assets/Scripts/Test.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using WebSocketSharp;
 
 public class Test : MonoBehaviour {
 
@@ -23,6 +24,20 @@ public class Test : MonoBehaviour {
 		}
 	};
 
+	public class PlayerAnswers
+	{
+		public int userId;
+		public int code = 2;
+		public string[] answers;
+		public int[] idOrder;
+
+		public PlayerAnswers(int size){
+			userId = GameObject.Find ("PlayerInfo").GetComponent<PlayerData> ().id;
+			answers = new string[size];
+			idOrder = new int[size];
+		}
+	}
+
 	private GameObject toggles;
 	private GameObject longAnsw;
 
@@ -31,8 +46,7 @@ public class Test : MonoBehaviour {
 	private int curQuestion;
 
 	private Question[] questions;
-	private string[] playerAnswers;
-	private int[] idOrder;
+	private PlayerAnswers playerAnswers;
 
 	void Start () {
 		QuestionsCreation ();
@@ -42,6 +56,8 @@ public class Test : MonoBehaviour {
 			GameObject.Find ("Title").GetComponent<Text> ().text = "Квадратные уравнения";	
 		else if (Menu.castle == 3)
 			GameObject.Find ("Title").GetComponent<Text> ().text = "Графики";
+		else if (Menu.castle == 4)
+			GameObject.Find ("Title").GetComponent<Text> ().text = "Словарный запас";
 		//TODO: don't forget to fill it!!!
 
 		curQuestion = -1;
@@ -56,48 +72,59 @@ public class Test : MonoBehaviour {
 	void QuestionsCreation()
 	{
 		questions = new Question[numOfQuestions]; 
-		playerAnswers = new string[numOfQuestions];
-		idOrder = new int[numOfQuestions];
+		playerAnswers = new PlayerAnswers(numOfQuestions); 
+		//playerAnswers = new string[numOfQuestions];
+		//idOrder = new int[numOfQuestions];
 
 		questions [0] = new Question (1, 823, "Question", new string[] { "a1","a2","a3", "a4" });
 		questions [1] = new Question (2, 172, "Questionn", new string[] { "aa1", "aa2", "aa3", "aa4" });
 		questions [2] = new Question (3, 45, "Questionnn", null);
 
 		for (int i = 0; i < questions.Length; i++)
-			idOrder [i] = questions [i].id;
+			playerAnswers.idOrder [i] = questions [i].id;
 
 		for (int i = 0; i < questions.Length; i++)
-			playerAnswers [i] = "";
+			playerAnswers.answers [i] = "";
 	}
 
 	public void PrevQuestion(){
 		// Saving player answer
 		if (qtype == 3)
-			playerAnswers [curQuestion] = GameObject.Find ("PlayerInput").GetComponent<Text> ().text;
+			playerAnswers.answers [curQuestion] = GameObject.Find ("PlayerInput").GetComponent<Text> ().text;
 		else
-			playerAnswers [curQuestion] = AnswerReadingTog ();
+			playerAnswers.answers [curQuestion] = AnswerReadingTog ();
 		
 		curQuestion--;
 
 		qtype = questions [curQuestion].type;
 		ChangeLayout (qtype);
 
-		Debug.Log ("0 = " + playerAnswers [0] + ", 1 = " + playerAnswers [1] + ", 2 = " + playerAnswers [2]);
+		Debug.Log (playerAnswers.idOrder[0] + " = " + playerAnswers.answers [0] + ", " + playerAnswers.idOrder[1] + " = " + playerAnswers.answers [1] + ", " + playerAnswers.idOrder[2] + " = " + playerAnswers.answers [2]);
 	}
 
 	// Checking type of next question
 	public void NextQuestion() {		
 		// Saving player answer if it's not test start
 		if (qtype == 3 && curQuestion != -1)
-			playerAnswers [curQuestion] = GameObject.Find ("PlayerInput").GetComponent<Text> ().text;
+			playerAnswers.answers [curQuestion] = GameObject.Find ("PlayerInput").GetComponent<Text> ().text;
 		else if (curQuestion != -1)
-			playerAnswers [curQuestion] = AnswerReadingTog ();
+			playerAnswers.answers [curQuestion] = AnswerReadingTog ();
 		
 		curQuestion++;
 
 		if (curQuestion + 1 > numOfQuestions){
 			// Finishing test
 			GameObject.Find ("PlayerInfo").GetComponent<PlayerData> ().completedTests [Menu.castle - 1] = true;
+
+			#region SOCKET STUFF
+			/*
+			WebSocket socket = new WebSocket("ws://127.0.0.1:16000");
+			socket.Connect();
+			string jsonmessage = JsonUtility.ToJson(playerAnswers);
+			socket.Send(jsonmessage);
+			*/
+			#endregion
+
 			SceneManager.LoadScene ("Menu"); // TODO sending answers to server
 		}
 		else {
@@ -105,7 +132,7 @@ public class Test : MonoBehaviour {
 			ChangeLayout (qtype);
 		}
 
-		Debug.Log ("0 = " + playerAnswers [0] + ", 1 = " + playerAnswers [1] + ", 2 = " + playerAnswers [2]);
+		Debug.Log (playerAnswers.idOrder[0] + " = " + playerAnswers.answers [0] + ", " + playerAnswers.idOrder[1] + " = " + playerAnswers.answers [1] + ", " + playerAnswers.idOrder[2] + " = " + playerAnswers.answers [2]);
 	}
 
 	string AnswerReadingTog()
@@ -155,7 +182,7 @@ public class Test : MonoBehaviour {
 			toggles.SetActive(false);
 			longAnsw.SetActive (true);
 
-			GameObject.Find ("PlayerInput").GetComponent<Text> ().text = playerAnswers [curQuestion];
+			GameObject.Find ("PlayerInput").GetComponent<Text> ().text = playerAnswers.answers [curQuestion];
 		}
 
 		if (qtype == 2) {
@@ -202,13 +229,13 @@ public class Test : MonoBehaviour {
 	void TogglesSwitch(Toggle answer1, Toggle answer2, Toggle answer3, Toggle answer4)
 	{
 		answer1.isOn = answer2.isOn = answer3.isOn = answer4.isOn = false;
-		if (playerAnswers [curQuestion].Contains ("1"))
+		if (playerAnswers.answers [curQuestion].Contains ("1"))
 			answer1.isOn = true;
-		if (playerAnswers [curQuestion].Contains ("2"))
+		if (playerAnswers.answers [curQuestion].Contains ("2"))
 			answer2.isOn = true;
-		if (playerAnswers [curQuestion].Contains ("3"))
+		if (playerAnswers.answers [curQuestion].Contains ("3"))
 			answer3.isOn = true;
-		if (playerAnswers [curQuestion].Contains ("4"))
+		if (playerAnswers.answers [curQuestion].Contains ("4"))
 			answer4.isOn = true;
 	}
 }
---
+++ Learning Castle/Assets/Scripts/WebSockets.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using WebSocketSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class WebSockets : MonoBehaviour {
+
+    public WebSocket socket;
+    public bool isSocket = false;
+    //User user = new User();
+
+    /*public class User
+    {
+        public int status;
+        public int id;
+
+        public void Clear()
+        {
+           
+        }
+    }*/
+
+    private class Message
+    {
+		public int code = 1;
+        public string login;
+        public string password;
+    }
+
+    private void Update()
+    {
+		if (Input.GetKeyDown (KeyCode.Return))
+			GameObject.Find ("Enter").GetComponent<Button> ().onClick.Invoke();
+    }
+
+    public void auth()
+    {
+		if (isSocket) {
+			socket = new WebSocket ("ws://127.0.0.1:16000");
+			socket.Connect ();
+			string login = GameObject.Find ("Login").GetComponent<InputField> ().text;
+			string password = GameObject.Find ("Password").GetComponent<InputField> ().text;
+			Message message = new Message ();
+			message.login = login;
+			message.password = password;
+			string jsonmessage = JsonUtility.ToJson (message);
+			socket.Send (jsonmessage);
+			PlayerData user = GameObject.Find ("PlayerInfo").GetComponent<PlayerData> ();
+
+			socket.OnMessage += (sender, e) => {
+				Debug.Log (e.Data);
+
+				JsonUtility.FromJsonOverwrite (e.Data, user);
+			};
+
+			System.Threading.Thread.Sleep (500);
+			if (user.status == 1) {
+				Debug.Log ("OK");
+
+				Debug.Log (user.toString ());
+				Debug.Log (user.gender);
+
+				SceneManager.LoadScene ("Subjects");
+			} else {
+				Debug.Log ("NOT OK");
+				socket.Close ();
+				//Debug.Log(user.status);
+				//Debug.Log(user.id);
+			}
+		} else {
+			GameObject.Find ("LevelManager").GetComponent<LevelManager> ().LoadLevel ("Subjects");
+		}
+    }
+
+}
