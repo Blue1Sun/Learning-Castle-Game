bd156cb68bcfccce3158410102aafd73f9e3b2f4
BlueSun <whitepineapple69@gmail.com>
2018-04-07 19:56:09 +0300

    Graphs minigame is finished for parabolas

--- Learning Castle/Assets/Scripts/Graphs.cs
+++ Learning Castle/Assets/Scripts/Graphs.cs
@@ -1,41 +1,173 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Graphs : MonoBehaviour {
 
 	public int numOfDots;
 	public float frequency = 0.2f;
-	//public float force;
 
-	public GraphType graphType;
-	public enum GraphType {Parabola, Linear, Sqrt};
+	//public GraphType graphType;
+	//public enum GraphType {Parabola, Linear, Sqrt};
+	public GameObject jumper;
 
+	private float[,] playerArr;
+
+
+
+	private int curRound;
+	private int numOfRounds = 10;
+	private int score;
+	private bool buttonWerePressed;
+	private int[] aArr;
+	private float[] kArr;
 
 	void Start() {
-		
+		curRound = 0;
+		score = 0;
+
+		playerArr = new float[,] { { -2, 4 }, { 0, 0 }, { 0, 0 }, { 0, 0 } };
+		NextRound ();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (graphType == GraphType.Parabola)
-			ParabolaGraph ();
-		if (graphType == GraphType.Linear)
-			LinearGraph ();
-		if (graphType == GraphType.Sqrt)
-			SqrtGraph ();		
+		//if (graphType == GraphType.Parabola)
+			//ParabolaGraph (0, 1);
+		//if (graphType == GraphType.Linear)
+			//LinearGraph ();
+		//if (graphType == GraphType.Sqrt)
+			//SqrtGraph ();		
 	}
 
-	void ParabolaGraph(){
+	public void NextRound(){
+		curRound++;
+		buttonWerePressed = false;
+
+		if (curRound <= numOfRounds) {
+			Destroy (GameObject.Find ("Jumper"));
+			foreach (Transform child in GameObject.Find("Dots").transform)
+				Destroy (child.gameObject);
+			GameObject newJumper = GameObject.Instantiate (jumper, jumper.transform.position, Quaternion.identity);
+			newJumper.name = "Jumper";
+
+			foreach (Transform child in GameObject.Find("Canvas").transform)
+				child.tag = "Untagged";
+
+			GameObject.Find ("Rounds").GetComponent<Text> ().text = curRound + " / " + numOfRounds;
+			GameObject.Find ("Score").GetComponent<Text> ().text = score.ToString ();
+			RandomGraphs ();
+		} 
+		else {
+			GameEnd ();
+		}
+	}
+
+	void GameEnd(){
+		Debug.Log ("Game end");
+		/*string resultMessage;
+
+		window.SetActive (true);
+
+		if (score <= numOfRounds / 2)
+			resultMessage = "Вам стоит попробовать еще раз. \r\n\r\nВы набрали " + score + " очков.";
+		else if (score > numOfRounds / 2 && score < numOfRounds)
+			resultMessage = "Отличная работа! \r\n\r\nВы набрали " + score + " очков.";
+		else 
+			resultMessage = "Потрясающий результат, Вы не сделали ни одной ошибки! \r\n\r\nВы набрали " + score + " очков.";
+		
+		PlayerData playerData = GameObject.Find ("PlayerInfo").GetComponent<PlayerData> ();
+		if (score > playerData.MinigameRecord [Menu.castle - 1])
+			playerData.MinigameRecord [Menu.castle - 1] = score;
+		
+		GameObject.Find("ResultMessage").GetComponent<Text>().text = resultMessage;
+		*/
+	}
+
+	void RandomGraphs(){
+		aArr = new int[3];
+		kArr = new float[3];
+
+		int a = 0;
+		float k  = 0;
+
+		for (int i = 0; i < 3; i++) {
+			do {
+				a = Random.Range (-3, 3);
+				k = Random.Range (1, 5) * 0.5f;
+			} while ((aArr [0] == a && kArr [0] == k) || (aArr [1] == a && kArr [1] == k));
+			aArr [i] = a;
+			kArr [i] = k;
+
+			if (a < 0)
+				GameObject.Find ((i + 1) + " Graph").GetComponentInChildren<Text> ().text = "y = " + k + "x² - " + (-a);
+			else if (a == 0)
+				GameObject.Find ((i + 1) + " Graph").GetComponentInChildren<Text> ().text = "y = " + k + "x²";
+			else if (a > 0)
+				GameObject.Find ((i + 1) + " Graph").GetComponentInChildren<Text> ().text = "y = " + k + "x² + " + a;					
+		}
+		int correctGraph = Random.Range (1, 4);
+		GameObject.Find (correctGraph + " Graph").tag = "CorrectGraph";
+
+		ParabolaGraph (aArr [correctGraph - 1], kArr [correctGraph - 1]);
+	}
+
+	public float MinXSqrt(){
+		return playerArr [0, 0];
+	}
+
+	public void ButtonPressed(int num){
+		GameObject button = GameObject.Find (num + " Graph");
+		if (button.tag == "CorrectGraph" && !buttonWerePressed)
+			score++;
+		if (!buttonWerePressed) {
+			ParabolaGraphPlayer (aArr [num - 1], kArr [num - 1]);
+
+			GameObject.Find ("Jumper").GetComponent<JumperController> ().moving = true;
+			GameObject.Find ("Jumper").GetComponent<Transform> ().position += Vector3.right * 2 * Time.deltaTime;
+		}
+
+		buttonWerePressed = true;
+	}
+
+	void ParabolaGraphPlayer(int a , float k){
+		float x = -3.5f;
+
+		do {
+			float y = x * x * k + a;
+			if (y <= 3.5f && y >= -3.5f){
+
+				JumperController jumper = GameObject.Find ("Jumper").GetComponent<JumperController>();
+				JumperController.Coordinate xy = new JumperController.Coordinate(x,y);
+				jumper.playerCoord.Add(xy);
+
+				//lineRenderer.SetPosition (num-1, new Vector3 (x, y, 0));
+			}
+			x += 0.05f;
+		} while (x <= 3.5f);
+	}
+
+	void ParabolaGraph(int a , float k){
 		LineRenderer lineRenderer = this.GetComponent<LineRenderer> ();
 
-		lineRenderer.numPositions = numOfDots;
-		float x = (-numOfDots / 2 + 0.5f) * frequency;
-		for (int index = 0; index < numOfDots; index++) {
-			float y = x * x;
-			lineRenderer.SetPosition (index, new Vector3(x, y, 0));
-			x += frequency;
-		}
+		//lineRenderer.numPositions = numOfDots;
+		//float x = (-numOfDots / 2 + 0.5f) * frequency;
+		float x = -3.5f;
+		int num = 0;
+			
+		//for (int index = 0; index < numOfDots; index++) {
+		do {
+			float y = x * x * k + a;
+			if (y <= 3.5f && y >= -3.5f){
+				num++;
+				lineRenderer.numPositions = num;
+				lineRenderer.SetPosition (num-1, new Vector3 (x, y, 0));
+			}
+			x += 0.1f;
+		} while (x <= 3.5f);
+		//}
 	}
 
 	void LinearGraph(){
@@ -60,9 +192,5 @@ public class Graphs : MonoBehaviour {
 			index++;
 		}
 	}
-
-	/*public void LaunchJumper(){
-		GameObject.Find ("Jumper").GetComponent<Rigidbody2D>().velocity += Vector2.right * force;
-	}*/
 					
 }
--- Learning Castle/Assets/Scripts/JumperController.cs
+++ Learning Castle/Assets/Scripts/JumperController.cs
@@ -3,32 +3,98 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class JumperController : MonoBehaviour {
+
+	public class Coordinate{
+		private float x; 
+		private float y;
+
+		public float X{
+			get	{ return x; }
+			set	{ x = value; }
+		}
+
+		public float Y{
+			get	{ return y; }
+			set	{ y = value; }
+		}
+
+		public Coordinate(float x, float y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+	public List<Coordinate> playerCoord;
 	
 	public GameObject dotPrefab;
+	public bool moving;
 
-	private bool jumperJumping;
+	private Graphs minigame;
+
+	private bool movingToGraph;
+	private bool drawingGraph;
+	private Vector2 zeroPos;
 
 	private float x1;
 	private float x1Max;
 
+	private int indexDot;
+
 	// Use this for initialization
 	void Start () {
-		jumperJumping = false;
+		playerCoord = new List<Coordinate>{ };
+		moving = false;
+		movingToGraph = false;
+		drawingGraph = false;
+
+
+		zeroPos = GameObject.Find ("MiniGame").GetComponent<Transform> ().position;
+		minigame = GameObject.Find ("MiniGame").GetComponent<Graphs> ();
+
 		x1 = 0;		
 		x1Max = 0;
+		indexDot = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (jumperJumping && x1 < x1Max) {
+		if (movingToGraph) {
+			// Клетка == 0.5f
+			// Центр: 4.7f x ; -0.26f y
+			// Уголок: 1.2f x ; 3.23f y
+			float step = 4 * Time.deltaTime;
+			float startX = playerCoord [0].X;
+			float startY = playerCoord [0].Y;
+
+			startX = zeroPos.x + startX;
+			startY = zeroPos.y + startY;
+			Vector2 startPos = new Vector3 (startX, startY, 0);
+
+			transform.position = Vector3.MoveTowards(transform.position, startPos, step);
+			if (transform.position.x == startPos.x && transform.position.y == startPos.y) {
+				movingToGraph = false;
+				drawingGraph = true;
+			}
+		}
+		if (drawingGraph && indexDot < playerCoord.Count-1)
+		{
 			//for (float x1 = -2; x1 < 2; x1 +=  0.01f) {
-			float y1 = x1 * x1;
+			//float y1 = x1 * x1;
 
-			float x2 = x1 + 0.05f;
-			float y2 = x2 * x2;
+			//float x2 = x1 + 0.05f;
+			//float y2 = x2 * x2;
 
-			float diffX = x2 - x1;
-			float diffY = y2 - y1;
+			//float diffX = x2 - x1;
+			//float diffY = y2 - y1;
+
+			//float posX = this.transform.position.x + diffX;
+			//float posY = this.transform.position.y + diffY;
+
+			//print (x1 + " " + y1 + " " + x2 + " " + y2);
+
+			float diffX = playerCoord [indexDot + 1].X - playerCoord [indexDot].X;
+			float diffY = playerCoord [indexDot + 1].Y - playerCoord [indexDot].Y;
 
 			float posX = this.transform.position.x + diffX;
 			float posY = this.transform.position.y + diffY;
@@ -38,21 +104,27 @@ public class JumperController : MonoBehaviour {
 			GameObject dot = GameObject.Instantiate (dotPrefab, new Vector3 (posX, posY, 0), Quaternion.identity) as GameObject;
 			dot.transform.parent = GameObject.Find ("Dots").GetComponent<Transform>();
 
-			x1 += 0.05f;
+			indexDot++;
 
+			//x1 += 0.05f;
+		}
+		if (indexDot >= playerCoord.Count-1 && moving) {
+			minigame.NextRound ();
 		}
 	}
 
+	//Starting with moving
 	void OnTriggerStay2D(Collider2D collider) {
-		if (collider.name == "Start") {
+		if (collider.name == "Start" && moving == true) {
 			this.transform.position += Vector3.right * 2 * Time.deltaTime;
 		}
 	}
 
+	//Flying to start of graph
 	void OnTriggerEnter2D(Collider2D collider) {
-		if (collider.name == "Finish" && !jumperJumping) {
+		if (collider.name == "Finish" && !drawingGraph) {
 			//JumperGraph ();
-			jumperJumping = true;
+			movingToGraph = true;
 			x1 = -2; //TODO TAKE GRAPH FROM USER
 			x1Max = 2;
 
